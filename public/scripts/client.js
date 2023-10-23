2fa1f6c1b6682dc8f3b8049cdf276205224ee19e
Yordanos Gebreselasie <yordibege@gmail.com>
2023-10-23 11:32:03 -0600

    Refactor AJAX POST and GET request and Implement compos button functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,111 +1,91 @@
-
-$(document).ready(() =>{
-
+$(document).ready(() => {
   const renderTweets = function(tweets) {
-   const $tweetsContainer = $('#tweets-container');
-   $tweetsContainer.empty(); 
-
-  for (const tweet of tweets) {
-    const $tweetElement = createTweetElement(tweet);
-     $tweetsContainer.prepend($tweetElement);
-   }
- };
-
-const createTweetElement = function(tweet) {
-const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
+    const $tweetsContainer = $('#tweets-container');
+    $tweetsContainer.empty();
+    for (const tweet of tweets) {
+      const $tweetElement = createTweetElement(tweet);
+      $tweetsContainer.prepend($tweetElement);
+    }
   };
-  console.log('text');
-  const $tweet = $(`
-    <article class="tweet">
-      <header>
-        <div class="avatar">
-          <img src="${tweet.user.avatars}" alt="Profile Image">
+
+  const createTweetElement = function(tweet) {
+    const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
+
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <div class="avatar">
+            <img src="${tweet.user.avatars}" alt="Profile Image">
+          </div>
+          <div class="name-handle">
+            <h2 class="user-name">${tweet.user.name}</h2>
+            <p class="handle">${tweet.user.handle}</p>
+          </div>
+        </header>
+        <div class="tweet-content">
+          <!-- Apply XSS-escaped content to the <p> element -->
+          <p>${escape(tweet.content.text)}</p>
         </div>
-        <div class="name-handle">
-          <h2 class="user-name">${tweet.user.name}</h2>
-          <p class="handle">${tweet.user.handle}</p>
-        </div>
-      </header>
-      <div class="tweet-content">
-      <!--Apply XSS-escaped content to the <p> element-->
-        <p>${escape(tweet.content.text)}</p>
-      </div>
-      <footer>
-        <div class="icon">
-          <i class="far fa-comment"></i>
-          <span class="timeago">${timeago.format(tweet.created_at)}</span>
-        </div>
-        <div class="icon">
-          <i class="fas fa-retweet"></i>
-          <span></span>
-        </div>
-        <div class="icon">
-          <i class="far fa-heart"></i>
-          <span></span>
-        </div>
-        <div class="icon">
-          <i class="fas fa-share"></i>
-        </div>
-      </footer>
-    </article>
-  `);
-  return $tweet;
-}
+        <footer>
+          <div class="icon">
+            <i class="far fa-comment"></i>
+            <span class="timeago">${timeago.format(tweet.created_at)}</span>
+          </div>
+          <div class="icon">
+            <i class="fas fa-retweet"></i>
+            <span></span>
+          </div>
+          <div class="icon">
+            <i class="far fa-heart"></i>
+            <span></span>
+          </div>
+          <div class="icon">
+            <i class="fas fa-share"></i>
+          </div>
+        </footer>
+      </article>
+    `);
 
-const loadTweets = function() {
-  $.ajax({
-    method: "GET",
-    url: "/tweets",
-    dataType: "json",
-    success: function(tweets) {
-      renderTweets(tweets);
-    },
-     });
-};
+    return $tweet;
+  };
 
-// Call loadTweets initially
+  $('form').on('submit', function(event) {
+    event.preventDefault();
 
-loadTweets();
- 
+    // Get the tweet content from the form
+    const tweetText = $('#tweet-text').val();
+    const $errorMessage = $('.error-message');
+    $errorMessage.hide();
 
-$('form').on('submit', function(event) {
-   event.preventDefault();
+    // Check if the tweet content is empty or exceeds 140 characters
+    if (!tweetText || tweetText.trim() === "") {
+      $errorMessage.text("Tweet content cannot be empty.").slideDown();
+      return; // Stop further execution
+    }
 
-   // Get the tweet content from the form
-   const tweetText = $('#tweet-text').val();
+    if (tweetText.length > 140) {
+      $errorMessage.text("Tweet content is too long. Maximum 140 characters allowed.").slideDown();
+      return; // Stop further execution
+    }
 
-   // Check if the tweet content is empty or exceeds 140 characters
-   if (!tweetText) {
-     alert("Tweet content is empty. Please enter your tweet.");
-     return; // Do not submit the form
-   }
+     // Send an AJAX POST request
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: $(this).serialize(),}).then(loadTweets);
 
-   if (tweetText.length > 140) {
-     alert("Tweet is too long. Please keep it within 140 characters.");
-     return; // Do not submit the form
-   }
-
-   const data = $(this).serialize();
-  
-//   // Send an AJAX POST request
-   $.ajax({
-    method: "POST",
-    url: "/tweets",
-    data: data,
-    success: function() {
-      // After successfully creating the tweet, load and render tweets.
-      $('#tweet-text').val('');
-      loadTweets();
-     }
-        })
-      })
-         
-      setInterval(function() {
-        loadTweets();
-      }, 30000); 
     });
   
-       
+
+  const loadTweets = function() {
+    $("tweet-text").val("");
+    $.ajax('/tweets',{method:'GET'}).then(renderTweets);
+  };
+     
+  loadTweets();
+});
+
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,41 @@
 $(document).ready(function() {
   // --- our code goes here ---
 
-console.log("tThe composer-char-counter  file  has been loaded")
+//console.log("tThe composer-char-counter  file  has been loaded")
+
+$(".new-tweet").find("textarea").on("input", function() {
+  const maxLength = 140;
+  const currentLength = $(this).val().length;
+  const remainingChars = maxLength - currentLength;
+
+  // Update the counter value
+  $(this).closest(".new-tweet").find(".counter").text(remainingChars);
+
+  // Change the counter color if the limit is exceeded
+  if (remainingChars < 0) {
+    $(this).closest(".new-tweet").find(".counter").addClass("invalid");
+  } else {
+    $(this).closest(".new-tweet").find(".counter").removeClass("invalid");
+  }
 });
+
+
+const $newTweetSection = $('.new-tweet');
+  const $composeButton = $('.compose-button');
+
+  // Hide the new-tweet section initially
+  $newTweetSection.slideUp(0);
+
+  // Toggle the new-tweet section on button click
+  $composeButton.click(() => {
+    $newTweetSection.slideToggle(200, function () {
+      // Automatically focus on the tweet text area when the section slides down
+      if ($newTweetSection.is(':visible')) {
+        $('#tweet-text').focus();
+      }
+    });
+  });
+
+
+});
+
